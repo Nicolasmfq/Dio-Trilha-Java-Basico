699b69b62532a8998b9f355241f0f8f6d2cf2750
Nicolasmfq <nicolas.engsoftware@gmail.com>
2023-02-06 15:13:12 -0300

    Operadores v2

---
+++ Tipos-Variaveis/src/Semana1/Operadores.java
@@ -0,0 +1,53 @@
+public class Operadores {
+    
+    public static void main(String[] Args) {
+    // testando incremento e decremento Unário
+        
+            /*int numero = 5;
+    
+            System.out.println(-- numero);
+            System.out.println(++ numero);
+            */
+
+    // testando ! Negação 
+            /*boolean variavel = true;
+
+            System.out.println(!variavel);
+
+            variavel = !variavel;
+            System.out.println(variavel);
+        */
+
+    //Ternário ? :
+        /*int a, b;
+        
+        a = 6;
+        b = 6;
+
+        int resultado = a==b ? 1 : 0;
+        
+        System.out.println(resultado);*/
+
+    //Relacional
+        /*int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+            System.out.println("Número 1 é igual ao Número 2?\n" + simNao);
+            System.out.println("Número 1 é diferente do Número 2\n" + simNao);*/
+        
+    //Lógicos
+        /*boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras.");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira.");
+        }
+        
+        System.out.println("Fim.");*/
+}
+}
